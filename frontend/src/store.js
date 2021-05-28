3f26d377fc0d3691e01784fe69c381bc6ad7f21d
daholakido <long.lhoang.1809@gmail.com>
2021-05-28 21:37:42 +0700

    Implement cart items

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,11 +1,19 @@
-import {applyMiddleware, combineReducers, compose, createStore} from 'redux';
+import { applyMiddleware, combineReducers, compose, createStore } from 'redux';
 import thunk from 'redux-thunk';
+import { cartReducer } from './reducers/cartReducer';
 import { productDetailsReducer, productListReducer } from "./reducers/productReducer";
 
-const initialState = {};
+const cartItems = localStorage.getItem('cartItems');
+
+const initialState = {
+    cart: {
+        cartItems:  cartItems ? JSON.parse(cartItems) : [],
+    }
+};
 const reducer = combineReducers({
     productList: productListReducer,
     productDetails: productDetailsReducer,
+    cart: cartReducer,
 });
 
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
