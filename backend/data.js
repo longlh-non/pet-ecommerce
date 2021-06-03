8ee4620d6b8bc2b28337af02d11e8467bb74069a
daholakido <long.lhoang.1809@gmail.com>
2021-06-03 12:45:02 +0700

    Create User in MongoDB

--- backend/data.js
+++ backend/data.js
@@ -1,4 +1,20 @@
+import bcrypt from 'bcryptjs';
+
 const data = {
+    users: [
+        {
+            name: 'admin',
+            email: 'admin@admin.com',
+            password: bcrypt.hashSync('1234', 8),
+            isAdmin: true
+        }, 
+        {
+            name: 'customer-1',
+            email: 'customer@admin.com',
+            password: bcrypt.hashSync('1234', 8),
+            isAdmin: false
+        },
+    ],
     products: [
         {
             _id: '1',
